dce5ce7a62886f1f61e9c248d4e084d208d4401c
Liul-Abera <liulaber43@gmail.com>
2024-08-31 15:10:55 -0400

    filter method in js

--- filter/index.js
+++ filter/index.js
@@ -1,10 +1,27 @@
+// .filter()=create a new array by filtering out  element 
 let names = ["LEUL", "Ananiya", "deva "];
-
 let filteredNames = names.filter(bros);
-
 function bros(brother) {
     // Trim the name to remove leading and trailing spaces, then check the first character
     return brother.trim().charAt(0) === brother.trim().charAt(0).toUpperCase();
 }
 
-console.log(filteredNames); // ["  LEUL"]
+console.log(filteredNames); // output   Leul , Ananiya ;
+const words =["apple ","orange","banana","kiwi","pomegrante","coconut"];
+let shortWords=words.filter(getShortWords);
+console.log(shortWords);
+let longWords =words.filter(getLongWords);
+console.log(longWords);
+
+function getShortWords(element){
+     return element.length<=6;
+
+
+}
+function getLongWords(element){
+    
+    return element.length>6;
+}
+
+
+
