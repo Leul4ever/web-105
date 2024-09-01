1c4de4badc3b7263ab436c7a854e495e7dff4450
Liul-Abera <liulaber43@gmail.com>
2024-09-01 17:12:45 -0400

    arrow function in js

---
+++ arrow/index.js
@@ -0,0 +1,10 @@
+// arrow function = a concise way to write a function expression good for
+// simple  function that you use only once (parameters )=>some code 
+
+const hello =(name ,age )=>{
+    console.log(`your name is ${name } and your age is ${age }`);
+
+} 
+hello("leul",23);
+
+
