7464146ae9cb474f3e5c1603b0f6960a549e9c26
Liul-Abera <liulaber43@gmail.com>
2024-09-02 05:56:21 -0400

    constructor

--- object/constructor/index.js
+++ object/constructor/index.js
@@ -1,17 +1,22 @@
 // constructor =special method for defining the properties and methods of objects
-
-
 function Car(make,model ,year,color){
     this.make=make,
     this.model=model,
     this.year=year,
     this.color=color,
     this.drive=function(){
-        console.log(`your drive the ${this.model}`)
+        console.log(`you drive the ${this.model}`)
     }
 
 }
 const car1=new Car("ford","mustang","2024","red");
 const car2= new Car("Chevrolet","Camaro","2025","blue");
 const Car3= new Car("leul ","charger","2016","silver")
+
+console.log(car1.model)
+console.log(car1.drive());
+
+
+
+
 
